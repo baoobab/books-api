497c76d068b03fad97e29663c4b21c0cc71522ab
baoobab <v.xxl.90@mail.ru>
2024-07-24 17:45:53 +0300

    fixes
    enum improvements
    and add some packages

--- api/src/shared/enums.ts
+++ api/src/shared/enums.ts
@@ -1,7 +1,8 @@
 export enum Roles { // Роли, и их численные представления
-  USER = 1,
-  MANAGER = 2,
-  ADMIN = 4,
+  NOT_CONFIRMED_USER = 0, // 000 | 0
+  USER = 1 << 0,          // 001 | 1
+  MANAGER = 1 << 1,       // 010 | 2
+  ADMIN = (1 << 3) - 1,   // 111 | 7, так как админ покрывает все роли
 }
 
 export enum ERRORS_MESSAGES {
@@ -10,4 +11,5 @@ export enum ERRORS_MESSAGES {
   INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR",
   NOT_FOUND = "NOT_FOUND",
   BAD_REQUEST = "BAD_REQUEST",
+  INVALID_CREDENTIALS = "INVALID_CREDENTIALS",
 }
