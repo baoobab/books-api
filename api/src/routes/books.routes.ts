59ef22f96b0eb12d39554794f8927245055554a2
baoobab <v.xxl.90@mail.ru>
2024-07-24 13:58:19 +0300

    books roles check

--- api/src/routes/books.routes.ts
+++ api/src/routes/books.routes.ts
@@ -1,16 +1,18 @@
 import {Router} from 'express';
 import {create, getAll, getById, update, remove} from '../controllers/books.controller';
+import {Roles} from "../shared/enums";
+import {jwtAuth} from "../middleware/auth";
 
 const router = Router()
 
-router.post('/', create)
+router.post('/', jwtAuth([Roles.ADMIN]), create)
 
 router.get("/", getAll)
 
 router.get("/:id", getById)
 
-router.put('/:id', update)
+router.put('/:id', jwtAuth([Roles.ADMIN]), update)
 
-router.delete('/:id', remove)
+router.delete('/:id', jwtAuth([Roles.ADMIN]), remove)
 
 export default router;
