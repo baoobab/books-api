165b9fe1b3c327841ceec95af5b7b8ca465b46ad
baoobab <v.xxl.90@mail.ru>
2024-07-24 19:03:30 +0300

    admin init fixes
    add exist checking

--- api/src/utils/db.init.ts
+++ api/src/utils/db.init.ts
@@ -1,6 +1,7 @@
 import pool from '../config/db';
 import {generateUUID, hashString} from "./utils.scripts";
 import {Roles} from "../shared/enums";
+import {User} from "../models/users.model";
 
 const createUsersTable = async () => {
   const query: string = `
@@ -31,25 +32,48 @@ const createBooksTable = async () => {
   await pool.query(query)
 }
 
+// const createAdminUser = async () => {
+//   const query: string = `
+//         INSERT INTO users (
+//         username,
+//         email,
+//         password,
+//         roleBits,
+//         confirmationToken
+//         )
+//         VALUES (
+//             '${process.env.ADMIN_USERNAME}',
+//             '${process.env.ADMIN_EMAIL}',
+//             '${await hashString(process.env.ADMIN_PASSWORD || "adminSecretPass")}',
+//             ${Roles.ADMIN},
+//             '${await generateUUID()}'
+//         );
+//     `
+//   await pool.query(query)
+// }
+
 const createAdminUser = async () => {
-  const query: string = `
-        INSERT INTO users (
-        username, 
-        email, 
-        password, 
-        roleBits, 
-        confirmationToken
-        )
-        VALUES (
-            '${process.env.ADMIN_USERNAME}',   
-            '${process.env.ADMIN_EMAIL}',   
-            '${await hashString(process.env.ADMIN_PASSWORD || "adminSecretPass")}',   
-            ${Roles.ADMIN},
-            '${await generateUUID()}'
-        );
+  const username = process.env.ADMIN_USERNAME || "admin"
+  const email = process.env.ADMIN_EMAIL || "admin@mail.admin"
+  const password = await hashString(process.env.ADMIN_PASSWORD || "adminSecretPass")
+  const roleBits = Roles.ADMIN
+  const confirmationToken = await generateUUID()
+
+  const existCheckResult = await pool.query<User>(
+    'SELECT * FROM users WHERE username = $1',
+    [username])
+  if (existCheckResult.rows.length) return;
+
+  const query = `
+        INSERT INTO users (username, email, password, roleBits, confirmationToken)
+        VALUES ($1, $2, $3, $4, $5);
     `
-  await pool.query(query)
-}
+
+  const values = [username, email, password, roleBits, confirmationToken]
+
+  await pool.query(query, values)
+};
+
 
 
 export const initDatabase = async () => {
